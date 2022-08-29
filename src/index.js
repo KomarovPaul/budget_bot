0a2ee59cf2f9f36f1ca020a8271a116451c4a60d
Pavel Komarov <pskomarov@avito.ru>
2022-08-29 15:25:22 +0300

    Init telegram bot

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const telegraf = require('telegraf');
+
+const bot = new telegraf.Telegraf(process.env.BOT_TOKEN);
+
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
